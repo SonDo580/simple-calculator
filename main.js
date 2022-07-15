0b9e884764cf5263a1abdd770a01535953161fac
Son Do <dominhson580@gmail.com>
2022-07-15 14:26:26 +0700

    Modify function name: show => addToExpression

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const numberButtons = document.querySelectorAll('.number');
-numberButtons.forEach(button => button.addEventListener('click', show));
+numberButtons.forEach(button => button.addEventListener('click', addToExpression));
 
 const operatorButtons = document.querySelectorAll('.operator');
-operatorButtons.forEach(button => button.addEventListener('click', show));
+operatorButtons.forEach(button => button.addEventListener('click', addToExpression));
 
 const operationDisplay = document.querySelector('.operation');
 const resultDisplay = document.querySelector('.result');
 
 operationDisplay.textContent = '';
 
-function show(e) {
+function addToExpression(e) {
     const button = e.target;
     operationDisplay.textContent += 
     `${button.classList.contains('operator') ? ' ' + button.textContent + ' ' : button.textContent}`;
